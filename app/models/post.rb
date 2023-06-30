32e64f386c0ba0b096945bd88f7bbbce9adfee41
simonkhartoum <simonkhartoum@gmail.com>
2023-06-30 11:48:21 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validate :clickbait?
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    
+    CLICKBAIT_PHRASES = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PHRASES.none? { |t| t.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
